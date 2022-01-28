1b68d6798a3b0d66409f661216f59081489eece0
Kyawatan <nico.vip@icloud.com>
2022-01-29 06:00:23 +0900

    【機能追加】　キー入力でプレイヤーが方向転換して1マス移動

--- Assets/Scripts/ThisGame/GameMain/GameMain.cs
+++ Assets/Scripts/ThisGame/GameMain/GameMain.cs
@@ -21,19 +21,19 @@ namespace GameMainSpace
 			// キー入力で移動
 			if (Input.GetKeyDown(KeyCode.W))
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_PlayerDirection.Forward); // 前
+				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Forward);	// 前
 			}
 			else if (Input.GetKeyDown(KeyCode.A))
             {
-				GameMainData.Player.Move(PlayerSpace.Player.E_PlayerDirection.Left); // 左
+				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Left);		// 左
 			}
 			else if (Input.GetKeyDown(KeyCode.S))
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_PlayerDirection.Right); // 右
+				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Back);		// 後ろ
 			}
 			else if (Input.GetKeyDown(KeyCode.D))
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_PlayerDirection.Back); // 後ろ
+				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Right);		// 右
 			}
 		}
 
--- Assets/Scripts/ThisGame/GameMain/Player/Player.cs
+++ Assets/Scripts/ThisGame/GameMain/Player/Player.cs
@@ -11,58 +11,103 @@ namespace GameMainSpace.PlayerSpace
 			bool CanMove( Vector3 nextMasu );
 		}
 
-		public enum E_PlayerDirection // プレイヤーの向き
+		public enum E_MoveDirection // プレイヤーの移動方向
 		{
 			Forward,
-			Left,
 			Right,
+			Left,
 			Back,
 		}
 
 		GameObject GameObject { get; }
 		IPlayer PlayerInterface { get; }
 
-		const int m_Masu = 1; // マスの大きさ（仮）
-		E_PlayerDirection m_eNowDirection;
-		Vector3 m_nowMasu;
-		Vector3 m_nextMasu;
-		bool m_isMove = false; // 現在動いているかどうかのフラグ
+		[SerializeField] float m_moveSpeed = 3.0f;		// 移動スピード
+		[SerializeField] float m_turnSpeed = 360.0f;	// 回転スピード
+
+		const int m_Masu = 1;		// マスの大きさ（仮）
+
+		bool m_isMove = false;		// 現在動いているかどうかのフラグ
+		bool m_isTurn = false;		// 現在回転しているかどうかのフラグ
+		Vector3 m_nextMasuPos;		// 移動先のマスの座標
+		Quaternion m_NextMasuRot;	// 移動先のマスへの回転量
 
 		public Player( GameObject gameObject , IPlayer playerInterface )
 		{
 			GameObject = gameObject;
 			PlayerInterface = playerInterface;
-			m_eNowDirection = E_PlayerDirection.Forward; // 最初は前を向いている
         }
 
 		public void Update()
 		{
-			if (m_isMove) MoveNextMasu();
+			if (m_isTurn)
+			{
+				TurnNextMasu();
+			}
+			else
+			{
+				if (m_isMove) MoveNextMasu();
+			}
 		}
 
-		public void Move(E_PlayerDirection eNextDirection)
+		public void Move(E_MoveDirection eNextDirection)
         {
-			m_nowMasu = GameObject.transform.position;
-			m_nextMasu = new Vector3(m_nowMasu.x, m_nowMasu.y, m_nowMasu.z + m_Masu);
-			if (PlayerInterface.CanMove(m_nextMasu))
+			if (m_isMove) return; // 移動中は操作不可
+
+			// 移動先のマスの座標を求める
+			Vector3 m_moveRight = new Vector3(m_Masu, 0f, 0f);
+			Vector3 m_moveForward = new Vector3(0f, 0f, m_Masu);
+			if (eNextDirection == E_MoveDirection.Forward)
 			{
-				// 移動可能なら移動フラグを立てる＆必要なら回転
-				m_isMove = true;
-				if (eNextDirection != m_eNowDirection) RotateNextMasu();
+				m_nextMasuPos = GameObject.transform.position + m_moveForward;	// 前
 			}
-        }
+			else if (eNextDirection == E_MoveDirection.Right)
+			{
+				m_nextMasuPos = GameObject.transform.position + m_moveRight;	// 右
+			}
+			else if (eNextDirection == E_MoveDirection.Left)
+			{
+				m_nextMasuPos = GameObject.transform.position - m_moveRight;	// 左
+			}
+			else if (eNextDirection == E_MoveDirection.Back)
+			{
+				m_nextMasuPos = GameObject.transform.position - m_moveForward;	// 後ろ
+			}
 
-		private void RotateNextMasu()
-        {
+			if (PlayerInterface.CanMove(m_nextMasuPos)) return; // 移動不可のマス
 
-        }
+			m_NextMasuRot = Quaternion.LookRotation(GameObject.transform.position - m_nextMasuPos, Vector3.up);
+			m_isMove = true;
+			m_isTurn = true;
+		}
+
+		private void TurnNextMasu()
+		{
+
+			if (GameObject.transform.rotation != m_NextMasuRot)
+			{
+				// プレイヤーの回転
+				GameObject.transform.rotation = Quaternion.RotateTowards(GameObject.transform.rotation, m_NextMasuRot, m_turnSpeed * Time.deltaTime);
+			}
+			else
+			{
+				// 回転し終わったら回転フラグを下ろす
+				m_isTurn = false;
+			}
+		}
 
 		private void MoveNextMasu()
 		{
-			// プレイヤーの移動
-			m_nowMasu = Vector3.MoveTowards(m_nowMasu, m_nextMasu, Time.deltaTime);
-			// 移動し終わったら移動フラグを下ろす
-			if (m_nowMasu == m_nextMasu) m_isMove = false;
-        }
+			if (GameObject.transform.position != m_nextMasuPos)
+			{
+				// プレイヤーの移動
+				GameObject.transform.position = Vector3.MoveTowards(GameObject.transform.position, m_nextMasuPos, m_moveSpeed * Time.deltaTime);
+			}
+			else
+			{
+				// 移動し終わったら移動フラグを下ろす
+				m_isMove = false;
+			}
+		}
 	}
 }
