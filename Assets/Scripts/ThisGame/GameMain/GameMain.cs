c2a6d844d23c5be8778f49a9f22acf8faca2fa31
Kyawatan <nico.vip@icloud.com>
2022-01-29 11:51:31 +0900

    【修正】　プレイヤー移動の修正、ろうそく所持数管理
    ・Move()関数の引数に移動先のマスの座標
    ・プレイヤーの移動終了時にFinishMove()を呼び出す
    ・ろうそく所持数のゲッターセッターGetSetCandleNum()
    ・ろうそくの所持数が0になるとZeroCandleCallBack()を呼び出す

--- Assets/Scripts/ThisGame/GameMain/GameMain.cs
+++ Assets/Scripts/ThisGame/GameMain/GameMain.cs
@@ -19,22 +19,24 @@ namespace GameMainSpace
 			CollisionController.GetInstance().Update();
 
 			// キー入力で移動
-			if (Input.GetKeyDown(KeyCode.W))
+			if (Input.GetKeyDown(KeyCode.W)) // 上
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Forward);	// 前
+				GameMainData.Player.Move(new Vector3(0f,0f,1f)); // (*)引数の座標は仮です。
 			}
-			else if (Input.GetKeyDown(KeyCode.A))
+			else if (Input.GetKeyDown(KeyCode.A)) // 左
             {
-				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Left);		// 左
+				GameMainData.Player.Move(new Vector3(-1f, 0f, 0f));
 			}
-			else if (Input.GetKeyDown(KeyCode.S))
+			else if (Input.GetKeyDown(KeyCode.S)) // 下
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Back);		// 後ろ
+				GameMainData.Player.Move(new Vector3(0f, 0f, -1f));
 			}
-			else if (Input.GetKeyDown(KeyCode.D))
+			else if (Input.GetKeyDown(KeyCode.D)) //右
 			{
-				GameMainData.Player.Move(PlayerSpace.Player.E_MoveDirection.Right);		// 右
+				GameMainData.Player.Move(new Vector3(1f, 0f, 0f));
 			}
+
+			GameMainData.Player.Update();
 		}
 
 	}
