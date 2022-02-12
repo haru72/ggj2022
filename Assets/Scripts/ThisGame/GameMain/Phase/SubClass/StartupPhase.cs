3dea1615fe8070df9d5149d8cecd22ebcf74a508
haru72 <governocean@gmail.com>
2022-02-12 11:48:23 +0900

    フィールド修正 画像差し替え GameMainSceneのみでの実行対応

--- Assets/Scripts/ThisGame/GameMain/Phase/SubClass/StartupPhase.cs
+++ Assets/Scripts/ThisGame/GameMain/Phase/SubClass/StartupPhase.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameMainSpace.PhaseSpace
 {
 	public class StartupPhase : PhaseBase
 	{
+		Coroutine _coroutine;
 		public StartupPhase( GameMainData gameMainData ) : base( gameMainData )
 		{
 			//最初アクティブじゃないとエラー
 			GameMainData.UIGameMainManager.GameUIOpen();
+			if( !FadeManager.IsActive() )
+			{
+				_coroutine = SystemController.GetInstance().SystemBehaviour.StartCoroutine( StartupCoroutine() );
+				SceneManager.LoadScene( "Fade" , LoadSceneMode.Additive );
+				SceneManager.LoadScene( "InputManager" , LoadSceneMode.Additive );
+			}
+		}
+		IEnumerator StartupCoroutine()
+		{
+			MyAudioController.GetInstance().AddLoadTarget_All();
+			yield return MyAudioController.GetInstance().LoadAsync();
+			_coroutine = null;
+			MyAudioController.GetInstance().PlayBGM( MyAudioController.BGMType.Title );
 		}
 
 		public override void Update()
 		{
-			if( FadeManager.IsFadeEnd() )
+			if( FadeManager.IsFadeEnd() && _coroutine == null )
 			{
 				GameMainData.UIGameMainManager.GameUIClose();
 
